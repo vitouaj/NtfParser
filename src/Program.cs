91045175af787e6a3237856e435cc27f88d00a60
hunvitousalesforce <hunvitousalesforce@gmail.com>
2024-02-13 16:21:36 +0700

    print result to console

--- src/Program.cs
+++ src/Program.cs
@@ -3,7 +3,16 @@
 var text1 = "[BE][FE][Urgent][BE][Gaming][QA] there is error";
 var title = new NotifcationTitle(text1);
 var channels = title.GetChannelsFromTitle();
-foreach (var ch in channels)
+Print(channels, text1);
+
+
+static void Print(HashSet<NotificationChannel> channels, string originTitle)
 {
-    Console.WriteLine(ch.Value.ToString());
+    Console.WriteLine($"Input \"{originTitle}\"");
+    Console.Write($"Receive channels: ");
+    foreach (var ch in channels)
+    {
+        Console.Write(ch.Value.ToString() + ", ");
+    }
+    Console.WriteLine();
 }
